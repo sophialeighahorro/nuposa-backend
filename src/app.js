47e63f292a944e6da7871ffa9c7c1dd2bccbffff
Joseph Brian <natividadjosephbrian@gmail.com>
2025-12-04 09:33:06 +0800

    feat: finish cat controller

--- src/app.js
+++ src/app.js
@@ -1,10 +1,13 @@
 import express from "express";
 import cors from "cors";
+import catController from "./controller/catController.js";
 
 const app = express();
 
 app.use(cors());
 app.use(express.json());
+
+app.use("/api/cats", catController);
 
 app.get("/", (req, res) => {
   console.log("Browser route successful.");
--- src/controller/catController.js
+++ src/controller/catController.js
@@ -1,7 +1,7 @@
 import express from "express";
 import Cat from "../models/Cat.js";
 import upload from "../config/multerConfig.js";
-
+import fs from "fs";
 const router = express.Router();
 
 router.get("/", async (req, res) => {
@@ -13,3 +13,78 @@ router.get("/", async (req, res) => {
   }
 });
 
+router.post("/", upload.single("image"), async (req, res) => {
+  try {
+    const { name, age, isAdopted, desc } = req.body;
+
+    const newCat = new Cat({
+      name,
+      age,
+      isAdopted,
+      desc,
+    });
+
+    if (req.file) {
+      newCat.image.data = fs.readFileSync(req.file.path);
+      newCat.image.contentType = req.file.mimetype;
+    }
+
+    const savedCat = await newCat.save();
+
+    if (req.file) fs.unlinkSync(req.file.path);
+    res.status(201).json(savedCat);
+  } catch (err) {
+    res.status(500).json({ message: err.message });
+  }
+});
+
+router.get("/:id", async (req, res) => {
+  try {
+    const cat = await Cat.findById(req.params.id);
+    if (!cat) return res.status(404).json({ message: "Cat not found" });
+  } catch (err) {
+    res.status(500).json({ message: err.message });
+  }
+});
+
+router.put("/:id", upload.single("image"), async (req, res) => {
+  try {
+    const { name, age, isAdopted, desc } = req.body;
+
+    // Find existing cat first
+    let cat = await Cat.findById(req.params.id);
+    if (!cat) return res.status(404).json({ message: "Cat not found" });
+
+    // Update text fields if they are provided
+    if (name) cat.name = name;
+    if (age) cat.age = age;
+    if (isAdopted !== undefined) cat.isAdopted = isAdopted;
+    if (desc) cat.desc = desc;
+
+    // Update Image ONLY if a new file is uploaded
+    if (req.file) {
+      cat.image.data = fs.readFileSync(req.file.path);
+      cat.image.contentType = req.file.mimetype;
+      fs.unlinkSync(req.file.path); // Cleanup
+    }
+
+    const updatedCat = await cat.save();
+    res.status(200).json(updatedCat);
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+});
+
+router.delete("/:id", async (req, res) => {
+  try {
+    const deletedCat = await Cat.findByIdAndDelete(req.params.id);
+
+    if (!deletedCat) return res.status(404).json({ message: "Cat not found" });
+
+    res.status(200).json({ message: "Cat deleted successfully" });
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+});
+
+export default router;
